27cdfee087a4fc2aca61ce12eb4936ea377fe119
Bunterninja2609 <121108968+Bunterninja2609@users.noreply.github.com>
2024-09-17 06:49:34 +0000

    uzewgaksghvddfkjhvfoixkhbvc jhkbcxhgbu gvçilxhc xgu

---
+++ script.js
@@ -0,0 +1,10 @@
+var waitTime = 10;
+var waitTimeInput = document.getElementById("waitTimeInput")
+function startTest() {
+    waitTime = Number(waitTimeInput.value)
+    rearrangeBoxes()
+}
+function rearrangeBoxes(){
+    setInterval(function () {}, 1000);
+}
+window.setInterval(fu)
